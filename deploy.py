632a2a7babba472f3d94b0db3d18377e73adeff3
geekyzk <geekyzk@163.com>
2019-05-22 18:03:19 +0800

    change deploy.py

--- deploy.py
+++ deploy.py
@@ -8,19 +8,19 @@ print(env_dist['ossHost'])
 auth = oss2.Auth(env_dist['accessKeyId'], env_dist['accessKeySecret'])
 # Endpoint以杭州为例，其它Region请按实际情况填写。
 bucket = oss2.Bucket(auth, env_dist['ossHost'], env_dist['bucketName'])
+filesPath = []
 def listFiles(path):
   rootdir = path
   base_list = os.listdir(rootdir)
-  filesPath = []
   for i in range(0,len(base_list)):
     path = os.path.join(rootdir, base_list[i])
     if os.path.isfile(path):
       filesPath.append(path)
     else:
       listFiles(path)
-  return filesPath
 root_path = os.path.join(os.getcwd(),'dist')
-files = listFiles(root_path)
+listFiles(root_path)
+files = filesPath
 for i in range(0,len(files)):
   filename = files[i].replace(root_path + '/','')
   result = bucket.put_object_from_file(filename, files[i])
